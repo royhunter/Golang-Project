2266702d2a3dcbbaa6f2b8997be3cb3b53f32aab
royluo <roy.luoye@hotmail.com>
2015-12-11 00:10:38 +0800

    add db operation

--- stock_trade.go
+++ stock_trade.go
@@ -6,6 +6,9 @@ import (
 	"encoding/json"
 	"net/http"
 	"io/ioutil"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"strconv"
 )
 
 var EndTimeSlotKey []string = []string{"09", "10"} 
@@ -38,25 +41,34 @@ func SiteContentGet(site string) []byte {
 	return b
 }
 
+func FloatToString(input_num float64) string {
+    // to convert a float number to a string
+    return strconv.FormatFloat(input_num, 'f', 6, 64)
+}
 
-func DecodeFromTrade(info []byte) {
+
+func DecodeFromTrade(info []byte, db *sql.DB) {
 
 	fmt.Println("DecodeFromTrade called")
 
 	//fmt.Println(string(info))
 
 	var data map[string]interface{}
+	var sql string
 
 	if err := json.Unmarshal(info, &data); err == nil {
 		fmt.Println("From ", data["begin"], "To ", data["end"])
 		//fmt.Println(data["zhubi_list"])
 		data_array := data["zhubi_list"].([]interface{})
 		fmt.Println("Total", len(data_array), "enties")
-		//for i, v := range data_array {
-			//entry := v.(map[string]interface{})
+		for _, v := range data_array {
+			entry := v.(map[string]interface{})
 			//fmt.Println(i, ": ", entry["PRICE"])
 			//TODO:insert into db
-		//}
+			sql = `insert into trade(TRADE_TYPE, PRICE, TURNOVER) values(` + FloatToString(entry["TRADE_TYPE"].(float64)) + `,` + FloatToString(entry["PRICE"].(float64)) + `,` + FloatToString(entry["TURNOVER_INC"].(float64)) + `);`
+			//fmt.Println(sql)
+			db.Exec(sql)
+		}
 	}
 
 }
@@ -66,12 +78,40 @@ func main() {
 
 	fmt.Println("hello,welcome")
 
-	target_site := TradeSiteGet(stock_code, EndTimeSlotKey[0], EndTimeSlotVal[EndTimeSlotKey[0]][1])
+
+	dbname := "./" + stock_code + ".db"
+	db, err := sql.Open("sqlite3", dbname)
+	if err != nil {
+		fmt.Println("test.db open failed")
+		fmt.Println(err)
+	}
+
+	fmt.Println(dbname, "open sucessfully")
+
+	sqlstring := `create table trade (TRADE_TYPE integer, PRICE text, TURNOVER integer);`
+	db.Exec(sqlstring)
+
+	target_site := TradeSiteGet(stock_code, EndTimeSlotKey[0], EndTimeSlotVal[EndTimeSlotKey[0]][0])
 
 	fmt.Println(target_site)
 	
 	trade_info := SiteContentGet(target_site)
 
-	DecodeFromTrade(trade_info)
+	DecodeFromTrade(trade_info, db)
+
+	sqlstring = `select * from trade;`
+
+	rows, err := db.Query(sqlstring)
+	defer rows.Close()
+
+	for rows.Next() {
+		var trade_type int 
+		var price float64
+		var turnover int 
+		err = rows.Scan(&trade_type, &price, &turnover)
+		fmt.Printf("%d, %f, %d\n", trade_type, price, turnover)
+	}
+
+	db.Close()
 
 }
