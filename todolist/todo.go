5376eff2bba1839e737cd424eeac79c886ba8bcb
royluo <roy.luoye@hotmail.com>
2016-03-03 22:03:31 +0800

    coding

--- todolist/todo.go
+++ todolist/todo.go
@@ -6,28 +6,21 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	//"crypto/md5"
-	//"strconv"
-	//"time"
-	//"io"
+	"strconv"
 )
 
-const (
-	INIT   = 1
-	FINISH = 2
-)
 
 type event_item struct {
 	Item   string
-	Status int
+	Finish bool
 	Id     string
 }
 
 var event_count = 2
 
-var event_list = map[string]event_item{
-	"0": event_item{Item: "aaa", Status: INIT, Id: "0"},
-	"1": event_item{Item: "bbb", Status: FINISH, Id: "1"},
+var event_list = map[string] *event_item {
+	"0": &event_item{Item: "aaa", Finish: false, Id: "0"},
+	"1": &event_item{Item: "bbb", Finish: false, Id: "1"},
 }
 
 func req_print(r *http.Request) {
@@ -46,54 +39,82 @@ func req_print(r *http.Request) {
 	fmt.Println(string(b))
 }
 
-func render(w http.ResponseWriter, el map[string]event_item) {
+func render(w http.ResponseWriter, el map[string] *event_item) {
 	t, _ := template.ParseFiles("todo.html")
 	t.Execute(w, el)
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method: ", r.Method)
+	fmt.Println("Index method: ", r.Method)
 	if r.Method == "GET" {
-		render(w, event_list)
-	} else {
-		//req_print(r)
-		r.ParseForm()
-		//text := r.FormValue("event")
-		text := r.FormValue("event")
-		//id := ev.Count
-		//ev.Count++
-		//ev_it := event_item{Item: text, Status: INIT, Id: id}
-		//ev.Event = append(ev.Event, ev_it)
-		fmt.Println(text)
 		render(w, event_list)
 	}
 }
 
 func New(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method: ", r.Method)
+	fmt.Println("New method: ", r.Method)
 	if r.Method == "POST" {
 		r.ParseForm()
 		text := r.FormValue("event")
-		//id := ev.Count
-		//ev.Count++
-		//ev_it := event_item{Item: text, Status: INIT, Id: id}
-		//ev.Event = append(ev.Event, ev_it)
-		fmt.Println(text)
+		if text == "" {
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
+		id := event_count
+		event_count++
+		event_list[strconv.Itoa(id)] = &event_item{
+			Item: text,
+			Finish: false,
+			Id: strconv.Itoa(id),
+		}
 		render(w, event_list)
 	}
 }
 
 func Delete(w http.ResponseWriter, r *http.Request) {
-
+	fmt.Println("Delete  method: ", r.Method)
+	if r.Method == "GET" {
+		r.ParseForm()
+		id := r.FormValue("id")
+		delete(event_list, id)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
 }
 
 func Undo(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Undo method: ", r.Method)
+	if r.Method == "GET" {
+		r.ParseForm()
+		id := r.FormValue("id")
+		value, ok := event_list[id] 
+		if ok {
+			value.Finish = false
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
+	}
+}
+
+func Finish(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Finish  method: ", r.Method)
+	if r.Method == "GET" {
+		r.ParseForm()
+		id := r.FormValue("id")
+		value, ok := event_list[id] 
+		if ok {
+			value.Finish = true
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
+	}
 }
 
 func main() {
 	http.HandleFunc("/", Index)
 	http.HandleFunc("/new", New)
-	http.HandleFunc("/del", Delete)
+	http.HandleFunc("/finish", Finish)
+	http.HandleFunc("/delete", Delete)
 	http.HandleFunc("/undo", Undo)
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
