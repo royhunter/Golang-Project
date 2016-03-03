176241e055069a757935011cc41e6df512683957
royluo <royluo06@gmail.com>
2016-03-03 01:56:08 -0800

    coding

--- todolist/todo.go
+++ todolist/todo.go
@@ -1,53 +1,78 @@
-package main 
-
+package main
 
 import (
-	"net/http"
-	"log"
-	"html/template"
 	"fmt"
+	"html/template"
 	"io/ioutil"
+	"log"
+	"net/http"
 	//"crypto/md5"
 	//"strconv"
 	//"time"
 	//"io"
 )
 
-func req_print(r *http.Request) {
-    fmt.Println("Method: ", r.Method)
-    fmt.Println("path: ", r.URL.Path)
-    fmt.Println("Proto: ", r.Proto)
-    fmt.Println("scheme: ", r.URL.Scheme)
-    fmt.Println("Header: ", r.Header)
-    fmt.Println("Content-Type: ", r.Header["Content-Type"])
-    r.ParseForm()
-    fmt.Println("Form: ", r.Form)
-    fmt.Println("ContentLength: ", r.ContentLength)
-    fmt.Println("Body: ")
-    defer r.Body.Close()
-    b, _ := ioutil.ReadAll(r.Body)
-    fmt.Println(string(b))
+const (
+	INIT   = 1
+	FINISH = 2
+)
+
+type event_item struct {
+	Item   string
+	Status int
+	Id     string
 }
 
-func todo(w http.ResponseWriter, r *http.Request) {
+var event_count = 2
+
+var event_list = map[string]event_item{
+	"0": event_item{Item: "aaa", Status: INIT, Id: "0"},
+	"1": event_item{Item: "bbb", Status: FINISH, Id: "1"},
+}
+
+func req_print(r *http.Request) {
+	fmt.Println("Method: ", r.Method)
+	fmt.Println("path: ", r.URL.Path)
+	fmt.Println("Proto: ", r.Proto)
+	fmt.Println("scheme: ", r.URL.Scheme)
+	fmt.Println("Header: ", r.Header)
+	fmt.Println("Content-Type: ", r.Header["Content-Type"])
+	r.ParseForm()
+	fmt.Println("Form: ", r.Form)
+	fmt.Println("ContentLength: ", r.ContentLength)
+	fmt.Println("Body: ")
+	defer r.Body.Close()
+	b, _ := ioutil.ReadAll(r.Body)
+	fmt.Println(string(b))
+}
+
+func render(w http.ResponseWriter, el map[string]event_item) {
+	t, _ := template.ParseFiles("todo.html")
+	t.Execute(w, el)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method: ", r.Method)
 	if r.Method == "GET" {
-		t, _ := template.ParseFiles("todo.html")
-		t.Execute(w, nil)
+		render(w, event_list)
 	} else {
 		//req_print(r)
 		r.ParseForm()
-		for _, v := range r.Form["event"] {
-			fmt.Println("event: ", v)
-		}	
+		//text := r.FormValue("event")
+		text := r.FormValue("id")
+		//id := ev.Count
+		//ev.Count++
+		//ev_it := event_item{Item: text, Status: INIT, Id: id}
+		//ev.Event = append(ev.Event, ev_it)
+		fmt.Println(text)
+		render(w, event_list)
 	}
 }
 
 func main() {
-	http.HandleFunc("/", todo)
+	http.HandleFunc("/", Index)
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe failed: ", err)
 	}
-
 }
