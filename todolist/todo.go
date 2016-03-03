aad9a6e829d40cf0437a9f863b7d1d725bb3f802
royluo <roy.luoye@hotmail.com>
2016-03-04 00:16:32 +0800

    coding

--- todolist/todo.go
+++ todolist/todo.go
@@ -7,20 +7,80 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 
 type event_item struct {
 	Item   string
-	Finish bool
+	Finish int
 	Id     string
 }
 
 var event_count = 2
 
-var event_list = map[string] *event_item {
-	"0": &event_item{Item: "aaa", Finish: false, Id: "0"},
-	"1": &event_item{Item: "bbb", Finish: false, Id: "1"},
+func QueryAll() (map[string] *event_item) {
+	db, err := sql.Open("sqlite3", "./todo.sqlite")
+	if err != nil {
+		log.Fatal("QueryAll db open failed", err)
+	}
+	defer db.Close()
+
+	sqlstring := `create table TODO (ID text PRIMARY KEY, ITEM text, FINISH int);`
+	db.Exec(sqlstring)
+
+	sqlx := `SELECT * FROM TODO;`
+	rows, err := db.Query(sqlx)
+	defer rows.Close()
+
+	event_list := make(map[string] *event_item)
+	for rows.Next() {
+		var item string
+		var finish int
+		var id string
+		rows.Scan(&id, &item, &finish)
+		event_list[id] = &event_item{item, finish, id}
+	}
+
+	return event_list
+}
+
+func InsertItem(ei event_item) {
+	db, err := sql.Open("sqlite3", "./todo.sqlite")
+	if err != nil {
+		log.Fatal("InsertItem db open failed", err)
+	}
+	defer db.Close()
+
+	item := ei.Item
+	finish := ei.Finish
+	id := ei.Id
+
+	sql := `INSERT INTO TODO(ID, ITEM, FINISH) VALUES(` + id + `,` + item + `,` + strconv.Itoa(finish) + `)`
+	db.Exec(sql)
+}
+
+func DeleteItem(id string) {
+	db, err := sql.Open("sqlite3", "./todo.sqlite")
+	if err != nil {
+		log.Fatal("InsertItem db open failed", err)
+	}
+	defer db.Close()
+
+	sql := `DELETE FROM TODO WHERE ID=` + id
+	db.Exec(sql)
+}
+
+func UpdateItem(id string, fin int) {
+	db, err := sql.Open("sqlite3", "./todo.sqlite")
+	if err != nil {
+		log.Fatal("InsertItem db open failed", err)
+	}
+	defer db.Close()
+
+	sql := `UPDATE TODO SET finish=` + strconv.Itoa(fin) + ` WHERE ID=` + id
+	db.Exec(sql)
 }
 
 func req_print(r *http.Request) {
@@ -47,7 +107,8 @@ func render(w http.ResponseWriter, el map[string] *event_item) {
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Index method: ", r.Method)
 	if r.Method == "GET" {
-		render(w, event_list)
+		ev_list := QueryAll()
+		render(w, ev_list)
 	}
 }
 
@@ -62,12 +123,13 @@ func New(w http.ResponseWriter, r *http.Request) {
 		}
 		id := event_count
 		event_count++
-		event_list[strconv.Itoa(id)] = &event_item{
+		eii := event_item{
 			Item: text,
-			Finish: false,
+			Finish: 0,
 			Id: strconv.Itoa(id),
 		}
-		render(w, event_list)
+		InsertItem(eii)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
 }
 
@@ -76,7 +138,7 @@ func Delete(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		r.ParseForm()
 		id := r.FormValue("id")
-		delete(event_list, id)
+		DeleteItem(id)
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
@@ -87,12 +149,9 @@ func Undo(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		r.ParseForm()
 		id := r.FormValue("id")
-		value, ok := event_list[id] 
-		if ok {
-			value.Finish = false
-			http.Redirect(w, r, "/", http.StatusSeeOther)
-			return
-		}
+		UpdateItem(id, 0)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
 	}
 }
 
@@ -101,12 +160,9 @@ func Finish(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		r.ParseForm()
 		id := r.FormValue("id")
-		value, ok := event_list[id] 
-		if ok {
-			value.Finish = true
-			http.Redirect(w, r, "/", http.StatusSeeOther)
-			return
-		}
+		UpdateItem(id, 1)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
 	}
 }
 
